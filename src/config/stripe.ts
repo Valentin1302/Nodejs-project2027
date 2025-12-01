0e861dc4bffaa6c0e5ae945c9765367024ba09bb
Valentin1302 <valentingautier8@gmail.com>
2025-12-01 14:21:11 +0100

    stripe

--- src/config/stripe.ts
+++
@@ -1,4 +0,0 @@
-import Stripe from 'stripe';
-
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY || 'sk_test_xxx';
-export const stripe = new Stripe(stripeSecretKey, { apiVersion: '2025-10-29.clover' });
